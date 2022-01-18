3e4453aebcdafc4b8688309f7cdabcfe9d446df8
barbokupna <barbokupna@gmail.com>
2022-01-17 20:29:58 -0600

    Orig

---
+++ app/index.js
@@ -0,0 +1,7 @@
+const Web3 = require('web3');
+ //let web3 = new Web3('ws://127.0.0.1:8545');
+ let web3 = new Web3( 'wss://rinkeby.infura.io/ws/v3/c72941ff3c594156aa6ffda1433e851e');
+
+web3.eth.getChainId().then(function(chainId){
+	console.log(chainId);
+});
